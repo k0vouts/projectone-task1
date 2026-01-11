bbb0226b6b6bf732c6e9fa1681d7fd4c34e680dc
k0vouts <konvoutskoglou@gmail.com>
2026-01-11 21:06:46 +0100

    hot fix2

--- projectone/src/main/java/com/example/projectone/controller/HelloController.java
+++ projectone/src/main/java/com/example/projectone/controller/HelloController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.ui.Model;
 
-
 @Controller
 public class HelloController {
 
